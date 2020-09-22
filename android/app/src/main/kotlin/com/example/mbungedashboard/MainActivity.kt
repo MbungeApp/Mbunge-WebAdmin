87165c5bf62bab08ff86a5888ff26f1219e03199
Iampato <858wpwaweru@gmail.com>
2020-09-22 13:02:39 +0300

    Project init

---
+++ android/app/src/main/kotlin/com/example/mbungedashboard/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mbungedashboard
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color whiteColor = Color(0xFFf7f8f3);
+}
