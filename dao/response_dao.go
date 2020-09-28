ca558d65f5260f948d36c1df39348f69d27b74d6
= <=>
2020-09-28 17:37:28 +0300

    added response db operations

---
+++ dao/response_dao.go
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2020.
+ * MbungeApp Inc all rights reserved
+ */
+
+package dao
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"mbunge-admin/models/db"
+)
+
+type ResponseDaoInterface interface {
+	ReadAllResponse() []db.Response
+	ReadOneParticipation(responseId string) db.Response
+}
+
+type NewResponseDaoInterface struct {
+	Client *mongo.Client
+}
+
+func responseCollection(client *mongo.Client) *mongo.Collection {
+	return client.Database("mbunge").Collection("response")
+}
+
+func (r NewResponseDaoInterface) ReadAllResponse() []db.Response {
+	var responses []db.Response
+	cursor, err := responseCollection(r.Client).Find(context.Background(), bson.M{})
+	if err != nil {
+		return nil
+	}
+	err = cursor.All(context.Background(), &responses)
+	if err != nil {
+		return nil
+	}
+
+	return responses
+}
+func (r NewResponseDaoInterface) ReadOneParticipation(responseId string) db.Response {
+	objectID, _ := primitive.ObjectIDFromHex(responseId)
+	var response db.Response
+
+	err := responseCollection(r.Client).FindOne(context.Background(), bson.M{
+		"_id": objectID,
+	}).Decode(response)
+	if err != nil {
+		//log.Fatal(err)
+		if err == mongo.ErrNoDocuments {
+			return db.Response{}
+		}
+	}
+	return response
+}
