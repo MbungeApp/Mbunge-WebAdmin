aec7a412a6633a496d29abd34c703de2d0edb9b6
= <=>
2020-09-28 17:27:07 +0300

    added home handler

--- main.go
+++ main.go
@@ -5,6 +5,28 @@
 
 package main
 
+import (
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"net/http"
+
+	_homeHandler "mbunge-admin/v1/home/handler"
+)
+
 func main() {
-	
+	// Echo instance
+	e := echo.New()
+
+	// middleware
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete},
+	}))
+
+	_homeHandler.NewHomeHandler(e)
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.Logger.Fatal(e.Start(":" + "1323"))
 }
