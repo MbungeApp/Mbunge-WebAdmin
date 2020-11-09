2776f8f2aa3b77a6f2c2af9b42b8714affd2aa85
Iampato <858wpwaweru@gmail.com>
2020-11-09 22:45:52 +0300

    added form tags

--- models/request/add_participation.go
+++ models/request/add_participation.go
@@ -2,9 +2,9 @@ package request
 
 // PartipationRequest ..
 type PartipationRequest struct {
-	Name     string `json:"name"`
-	Sector   string `json:"sector"`
-	ExpireAt string `json:"expire_at"`
-	PostedBy string `json:"posted_by"`
-	Body     string `json:"body" bson:"body"`
+	Name     string `json:"name" form:"name"`
+	Sector   string `json:"sector" form:"sector"`
+	ExpireAt string `json:"expire_at" form:"expire_at"`
+	PostedBy string `json:"posted_by" form:"posted_by"`
+	Body     string `json:"body" bson:"body" form:"body"`
 }
