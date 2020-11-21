48871eda2cfee88e9d00c4543bc81a0a32e76601
Iampato <858wpwaweru@gmail.com>
2020-11-21 09:35:58 +0300

    added dashboard main metrics

--- v1/dashboard/handler/dashboard_handler.go
+++ v1/dashboard/handler/dashboard_handler.go
@@ -6,17 +6,33 @@
 package handler
 
 import (
+	"encoding/json"
 	"github.com/labstack/echo"
+	"mbunge-admin/v1/dashboard/service"
 	"net/http"
 )
 
-func NewDashboardHandler(e *echo.Echo) {
-	g := e.Group("/dashboard")
-	g.GET("", home)
+type dashboardHandler struct {
+	dashboardService service.DashboardServices
 }
 
-func home(c echo.Context) error {
+func NewDashboardHandler(e *echo.Echo, dashService service.DashboardServices) {
+	dashboardHandler := &dashboardHandler{
+		dashboardService: dashService,
+	}
+	g := e.Group("/dashboard")
+	g.GET("", dashboardHandler.home)
+}
+
+func (d dashboardHandler) home(c echo.Context) error {
+	var metrics interface{}
+	marshalMetrics, err := json.Marshal(d.dashboardService.GetMetrics())
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "error occurred")
+	}
+
+	err = json.Unmarshal(marshalMetrics, metrics)
 	return c.Render(http.StatusOK, "dashboard.html", map[string]interface{}{
-		"name": "Dolly!",
+		"data": metrics,
 	})
 }
