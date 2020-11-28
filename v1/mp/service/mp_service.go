1e94e9a766973f7e031b4b67d17b3594d8966875
Iampato <858wpwaweru@gmail.com>
2020-11-28 13:15:19 +0300

    added mp service interface

---
+++ v1/mp/service/mp_service.go
@@ -0,0 +1,14 @@
+package service
+
+import (
+	"mbunge-admin/models/db"
+	"mbunge-admin/models/request"
+)
+
+type MpService interface {
+	ViewAllMps() ([]db.MP, error)
+	AddMp(mp *request.MpRequest) error
+	EditMp(id string, mp *request.MpRequest) error
+	DeleteMp(id string) error
+	StartLiveSession(agoraId string) error
+}
