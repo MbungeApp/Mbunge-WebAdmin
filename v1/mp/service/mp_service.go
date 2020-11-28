0996af240541443a99c670b82298eec220735276
Iampato <858wpwaweru@gmail.com>
2020-11-28 13:47:08 +0300

    added mp service interface

--- v1/mp/service/mp_service.go
+++ v1/mp/service/mp_service.go
@@ -7,6 +7,7 @@ import (
 
 type MpService interface {
 	ViewAllMps() ([]db.MP, error)
+	ViewMpById(id string) db.MP
 	AddMp(mp *request.MpRequest) error
 	EditMp(id string, mp *request.MpRequest) error
 	DeleteMp(id string) error
