79f59bf736dace1af1a27132a3974d0bc42a705e
Iampato <858wpwaweru@gmail.com>
2021-02-08 16:05:31 +0300

    multipart issues

--- lib/models/add_mp.dart
+++ lib/models/add_mp.dart
@@ -4,46 +4,47 @@
 
 import 'dart:convert';
 
-AddMpModel addMpModelFromJson(String str) => AddMpModel.fromJson(json.decode(str));
+import 'dart:typed_data';
+
+AddMpModel addMpModelFromJson(String str) =>
+    AddMpModel.fromJson(json.decode(str));
 
 String addMpModelToJson(AddMpModel data) => json.encode(data.toJson());
 
 class AddMpModel {
-    AddMpModel({
-        this.name,
-        this.picture,
-        this.constituency,
-        this.county,
-        this.martialStatus,
-        this.dateOfBirth,
-        this.bio,
-    });
+  AddMpModel({
+    this.name,
+    this.picture,
+    this.constituency,
+    this.county,
+    this.martialStatus,
+    this.dateOfBirth,
+    this.bio,
+  });
 
-    String name;
-    String picture;
-    String constituency;
-    String county;
-    String martialStatus;
-    DateTime dateOfBirth;
-    String bio;
+  String name;
+  Uint8List picture;
+  String constituency;
+  String county;
+  String martialStatus;
+  DateTime dateOfBirth;
+  String bio;
 
-    factory AddMpModel.fromJson(Map<String, dynamic> json) => AddMpModel(
+  factory AddMpModel.fromJson(Map<String, dynamic> json) => AddMpModel(
         name: json["name"],
-        picture: json["picture"],
         constituency: json["constituency"],
         county: json["county"],
         martialStatus: json["martial_status"],
         dateOfBirth: DateTime.parse(json["date_of_birth"]),
         bio: json["bio"],
-    );
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, String> toJson() => {
         "name": name,
-        "picture": picture,
         "constituency": constituency,
         "county": county,
         "martial_status": martialStatus,
         "date_of_birth": dateOfBirth.toIso8601String(),
         "bio": bio,
-    };
+      };
 }
