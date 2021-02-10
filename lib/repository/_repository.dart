4d9f448f72ca01d54815dfcb6ba69e61b5b7b65d
Iampato <858wpwaweru@gmail.com>
2021-02-10 07:14:43 +0300

    added webinar repo

--- lib/repository/_repository.dart
+++ lib/repository/_repository.dart
@@ -6,13 +6,16 @@ import 'package:http/http.dart' as http;
 import 'package:mbungeweb/models/add_admin.dart';
 import 'package:mbungeweb/models/add_event.dart';
 import 'package:mbungeweb/models/add_mp.dart';
+import 'package:mbungeweb/models/add_webinar.dart';
 import 'package:mbungeweb/models/admin_model.dart';
 import 'package:mbungeweb/models/event.dart';
 import 'package:mbungeweb/models/metrics.dart';
 import 'package:mbungeweb/models/mp_model.dart';
+import 'package:mbungeweb/models/webinar.dart';
 import 'package:mbungeweb/utils/http.dart';
 
 part 'metrics_repo.dart';
 part 'events_repo.dart';
 part 'mp_repo.dart';
 part 'admin_repo.dart';
+part 'webinar_repo.dart';
