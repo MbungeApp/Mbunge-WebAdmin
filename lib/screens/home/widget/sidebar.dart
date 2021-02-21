aa3d7a4d524739e8df257177580e68fb9ce8d28b
Iampato <858wpwaweru@gmail.com>
2021-02-21 10:50:49 +0300

    added logout

--- lib/screens/home/widget/sidebar.dart
+++ lib/screens/home/widget/sidebar.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mbungeweb/cubit/authentication/authenticiation_cubit.dart';
 import 'package:mbungeweb/models/navigation.dart';
 import 'package:mbungeweb/models/user_model.dart';
+import 'package:mbungeweb/repository/_repository.dart';
+import 'package:mbungeweb/repository/shared_preference_repo.dart';
+import 'package:mbungeweb/widgets/restart_widget.dart';
 
 import 'sidebar_item.dart';
 
-class Sidebar extends StatelessWidget {
+class Sidebar extends StatefulWidget {
   const Sidebar({
     Key key,
     @required this.currentIndex,
@@ -15,71 +20,100 @@ class Sidebar extends StatelessWidget {
   final int currentIndex;
   final Function(int value) onTap;
   final UserModel userModel;
+
+  @override
+  _SidebarState createState() => _SidebarState();
+}
+
+class _SidebarState extends State<Sidebar> {
+  AuthenticiationCubit authenticiationCubit;
+
+  @override
+  void initState() {
+    authenticiationCubit = AuthenticiationCubit(
+      AdminRepo(),
+      SharedPreferenceRepo(),
+    );
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: double.infinity,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: EdgeInsets.fromLTRB(28, 50, 0, 80),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SizedBox(
-                  height: 15,
-                ),
-                Center(
-                  child: CircleAvatar(
-                    radius: 40,
-                    child: Icon(
-                      Icons.person,
-                      color: Colors.white,
+    return BlocProvider(
+      create: (context) => authenticiationCubit,
+      child: BlocListener(
+        cubit: authenticiationCubit,
+        listener: (context, state) {
+          if (state is AuthenticiationLoggedOut) {
+            RestartWidget.restartApp(context);
+          }
+        },
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.fromLTRB(28, 50, 0, 80),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height: 15,
                     ),
-                  ),
+                    Center(
+                      child: CircleAvatar(
+                        radius: 40,
+                        child: Icon(
+                          Icons.person,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text("Hello, ${widget.userModel.admin.name}"),
+                    SizedBox(
+                      height: 40,
+                    ),
+                  ],
                 ),
-                SizedBox(
-                  height: 10,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: sidebarData.map((e) {
+                    return SidebarItem(
+                      isActive: sidebarData.indexOf(e) == widget.currentIndex
+                          ? true
+                          : false,
+                      title: e.title,
+                      icon: e.icon,
+                      onTap: () {
+                        _navigate(sidebarData.indexOf(e));
+                      },
+                    );
+                  }).toList(),
                 ),
-                Text("Hello, ${userModel.admin.name}"),
-                SizedBox(
-                  height: 40,
-                ),
-              ],
-            ),
+              ),
+              Spacer(),
+              SidebarItem(
+                isActive: false,
+                title: "Logout",
+                icon: Icons.exit_to_app,
+                onTap: () {
+                  authenticiationCubit.logOutUser();
+                },
+              ),
+              SizedBox(height: 30)
+            ],
           ),
-          Padding(
-            padding: const EdgeInsets.only(left: 10.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: sidebarData.map((e) {
-                return SidebarItem(
-                  isActive:
-                      sidebarData.indexOf(e) == currentIndex ? true : false,
-                  title: e.title,
-                  icon: e.icon,
-                  onTap: () {
-                    _navigate(sidebarData.indexOf(e));
-                  },
-                );
-              }).toList(),
-            ),
-          ),
-          Spacer(),
-          SidebarItem(
-            isActive: false,
-            title: "Logout",
-            icon: Icons.exit_to_app,
-            onTap: () {
-              // loginCubit.logOutUser();
-            },
-          ),
-          SizedBox(height: 30)
-        ],
+        ),
       ),
     );
     //   },
@@ -88,6 +122,6 @@ class Sidebar extends StatelessWidget {
 
   void _navigate(int value) {
     if (value == null) return;
-    onTap(value);
+    widget.onTap(value);
   }
 }
